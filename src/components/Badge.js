7abed73ad16879017d7a2683c0d51f274feec999
joseoroz <joseoroz@JOSEOROZ-LAP.oradev.oraclecorp.com>
2019-06-04 22:56:42 -0500

    creating the first component

---
+++ src/components/Badge.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+class Badge extends React.Component {
+
+    render() {
+        return ( 
+           <div>
+               <div>
+                   <img src="" alt="Logo de la conferencia" />
+               </div>
+
+               <div>
+                   <img src="" alt="Avatar"/>
+                   <h1>Jose Luis <br/>Orozco</h1>
+               </div>
+
+               <div>
+                    <p>Fronend Engineer</p>
+                    <p>@jloroz10</p>
+                </div>
+
+                <div>
+                    #platziconf
+                </div>
+           </div>
+        );
+    }
+}
+
+
+        export default Badge;
