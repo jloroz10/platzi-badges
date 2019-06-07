75413ffe3dcbe0ed77241ad33847cbe2ee12383c
joseoroz <joseoroz@JOSEOROZ-LAP.oradev.oraclecorp.com>
2019-06-06 23:12:49 -0500

    manejo de eventos

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
-// const element = document.createElement('h1');
-// element.innerText = 'Hello, Platzi Badges!';
-
-// const container = document.getElementById('app');
-
-// container.appendChild(element);
-
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Badge from './components/Badge';
+import BadgeNew from './pages/BadgeNew.js';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './global.css'
@@ -18,9 +12,12 @@ const container = document.getElementById('app');
 
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(<Badge firstName="Jose" 
-                       lastName="Orozco" 
-                       jobTitle="Fronend Enginee" 
-                       twitter="jloroz10"
-                       footerMsg="#platziconf"
-                       avatarUrl="https://www.gravatar.com/avatar/1b11e78f239b7ccf5b9071ff6f4f87ff"/>, container);
+// ReactDOM.render(<Badge firstName="Jose" 
+//                        lastName="Orozco" 
+//                        jobTitle="Fronend Enginee" 
+//                        twitter="jloroz10"
+//                        footerMsg="#platziconf"
+//                        avatarUrl="https://www.gravatar.com/avatar/1b11e78f239b7ccf5b9071ff6f4f87ff"/>, container);
+
+
+ReactDOM.render(<BadgeNew />,container)
